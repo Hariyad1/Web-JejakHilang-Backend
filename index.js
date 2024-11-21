adab8740a4d75ceaa81724dde8a460befe383bfa
Hariyad1 <hariyadi3663@gmail.com>
2024-11-21 22:10:03 +0700

    Merubah url ke env

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ const imagekit = new ImageKit({
 //middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
@@ -47,23 +50,23 @@ app.use(fileUpload());
 
 //Image Upload
 app.post("/api/upload", (req, res) => {
-  console.log("Request files:", req.files); // Log untuk memeriksa isi req.files
+  console.log("Request files:", req.files); 
 
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
 
-  const file = req.files.file; // Menggunakan key 'file' untuk mendapatkan file dari request
-  const fileName = file.name; // Menggunakan nama asli file
+  const file = req.files.file; 
+  const fileName = file.name; 
 
   if (!file) {
     return res.status(400).send('File not found in request.');
   }
 
   imagekit.upload({
-    file: file.data, // file buffer
-    fileName: fileName, // Menggunakan nama asli file
-    folder: "/uploads" // folder di ImageKit
+    file: file.data, 
+    fileName: fileName, 
+    folder: "/uploads" 
   }, function(error, result) {
     if (error) {
       console.log("Error uploading to ImageKit:", error);
@@ -78,5 +81,4 @@ app.post("/api/upload", (req, res) => {
 app.listen(process.env.PORT,() => {
   connectDB()
   console.log("app is running on port "+process.env.PORT)
-
 }) 
