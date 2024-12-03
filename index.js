ffa1d7d09ee2b4d0feda1618a2945b84400eb0b0
Hariyad1 <hariyadi3663@gmail.com>
2024-12-03 18:35:24 +0700

    Memperbarui Cors Origin

--- index.js
+++ index.js
@@ -34,15 +34,24 @@ const imagekit = new ImageKit({
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+const allowedOrigins = [process.env.CORS_ORIGIN, 'https://web-jejak-hilang-frontend.vercel.app'];
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }))
+
 app.use(cookieParser())
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/posts",postRoute)
-app.use("/api/comments",commentRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+app.use("/api/comments", commentRoute)
 app.use("/api/admin", adminRoute)
 app.use(fileUpload());
 
