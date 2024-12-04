c43bfb546cafed82d838742a611426ea9d49f8d9
Hariyad1 <hariyadi3663@gmail.com>
2024-12-04 09:37:43 +0700

    Memperbarui origin

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const imagekit = new ImageKit({
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-const allowedOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : [];
+const allowedOrigins = ['https://web-jejak-hilang-frontend.vercel.app'];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
